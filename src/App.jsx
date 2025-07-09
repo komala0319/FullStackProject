d771009b4765b4633af6bc84dfc42eb693012e52
komala0319 <komalapalani2003@gmail.com>
2025-07-08 22:31:41 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <Route path="/" element ={<Navigate to="/Login"/>} />
       <Route path="/Register" element ={<Register/>} />
       <Route path="/Login" element ={<Login/>} />
-      {/* <Route path="/Success" element ={<Success/>} /> */}
+      <Route path="/Success" element ={<Success/>} />
     </Routes>
     </BrowserRouter>
       
